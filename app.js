a662f14f955d0e12b07977013794359a712c4ffb
James Davey <james.t.davey@gmail.com>
2019-01-20 12:18:13 -0800

    change writefile to not block code operations

--- app.js
+++ app.js
@@ -19,16 +19,15 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    // status code for redirect
-    res.statusCode = 302;
-    // redirect user to '/'
-    res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<h1>Hello</h1>');
