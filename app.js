656fe4686f104215d82195722d90bb8caa59ad65
James Davey <james.t.davey@gmail.com>
2019-01-21 11:33:28 -0800

    remove routes file, install express to production dependencies

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const http = require('http');
 
-const routes = require('./routes');
+const server = http.createServer();
 
-const server = http.createServer(routes);
-
-server.listen(3000, () => {
-  console.log('hello friend!');
-});
+server.listen(3000);
