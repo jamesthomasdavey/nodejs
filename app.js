597b29bdf94727310e00d7543910aed4b570aed5
James Davey <james.t.davey@gmail.com>
2019-01-22 11:33:36 -0800

    remove http import and use express method to run server instead

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer();
+app.use('/', (req, res, next) => {
+  console.log('bloop');
+  setTimeout(next, 3000);
+});
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+  console.log('add-product');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('root route');
+  res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000);
