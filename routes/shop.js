2aa9d0f86392e51441b04a1508c95e97bb904532
James Davey <james.t.davey@gmail.com>
2019-01-24 16:41:48 -0800

    add equivalent template files for handlebars and ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,18 @@
-const path = require('path');
 const express = require('express');
 
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, docTitle: 'Shop', path: '/' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    productCSS: true,
+    activeShop: true
+  });
 });
 
 module.exports = router;
