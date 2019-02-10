b928b268ee0a8bd5e305817213b04715ea4848d6
James Davey <james.t.davey@gmail.com>
2019-02-10 11:56:10 -0800

    display cart items on cart show page

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,7 +33,18 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart' });
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (let product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart', products: cartProducts });
+    });
+  });
 };
 
 exports.postCart = (req, res, next) => {
